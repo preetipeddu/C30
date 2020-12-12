66bbdb4f413646aa5427279056454c5dd56ceab5
preetipeddu <peddupreeti@gmail.com>
2020-12-12 13:26:43 -0500

    created a vanishing effect to the pigs

--- Pig.js
+++ Pig.js
@@ -2,6 +2,25 @@ class Pig extends BaseClass {
   constructor(x, y){
     super(x,y,50,50);
     this.image = loadImage("sprites/enemy.png");
-  }
+    this.visibility = 255;
 
+    //byte - 8 bits
+    //1 fully ON bye = 255 = 11111111
+    //1 fully OFF byte = 0 - 00000000
+  }
+  display(){
+    if(this.body.speed<3){
+      super.display();
+    }
+    else{
+      //do nothing
+      World.remove(world, this.body);
+      push();
+      this.visibility = this.visibility - 5;
+      // vanishing effect - tint 
+      tint(255, this.visibility);
+      image(this.image, this.body.position.x, this.body.position.y);
+      pop();
+    }
+  }
 };
